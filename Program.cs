3b0a8254c5178f1f183667fd5c4062b33642af97
Andrey Markov <arherower@outlook.com>
2022-09-21 19:37:35 +0300

    Доработано.

--- Program.cs
+++ Program.cs
@@ -6,28 +6,29 @@
         static void Main(string[] args)
         {
             int box;
-            Random rndBolt = new Random();
+            Random rndBolt = new Random();  //только один раз
             Random rndBox = new Random();
-            box = rndBox.Next(5, 50);
-            //Console.WriteLine(box);
+            box = rndBox.Next(5, 101);
+            Console.WriteLine(box);
             int [] boxArray = new int[box];
             for (int x = 0; x < boxArray.Length; x++)
             {
-                boxArray[x] = rndBolt.Next(100, 400);
-                //Console.WriteLine(boxArray[x]);
+                boxArray[x] = rndBolt.Next(100, 401);
+                Console.WriteLine(boxArray[x]);
             }
             int max;
             int min;
+            int middle;
             int index;
             int index2;
-            int index3;
+            int middleBox;
             max = GetMax2Element(boxArray, out index);
             min = GetMin2Element(boxArray, out index2);
-            mid = GetMid2Element(boxArray, out index3);
+            middleBox = GetMid2Element(boxArray);
 
-            Console.WriteLine("Max элемент: " + max + " в ящике " + index + ".");
-            Console.WriteLine("Min элемент: " + min + " в ящике " + index2 + ".");
-            Console.WriteLine("Mid элемент: " + mid + " в ящике " + index3 + ".");
+            Console.WriteLine("Max элемент: " + max + " в ящике " + (index + 1) + ".");
+            Console.WriteLine("Min элемент: " + min + " в ящике " + (index2 + 1) + ".");
+            Console.WriteLine("Mid элемент: " + boxArray[middleBox] + " в ящике " + (middleBox + 1) + ".");
         }
         private static int GetMax2Element(int[] boxArray, out int index)
         {
@@ -61,23 +62,36 @@
             }
             return min;
         }
-        private static int GetMid2Element(int[] boxArray, out int index3)
+        private static int GetMid2Element(int[] boxArray)
         {
-            int mid = 0;
-            int k = 0;
-            index3 = 0;
-            //for (int i = 0; i < boxArray.Length; i++)
-            //{
-
-            //}
-
-
-            //foreach (int element in boxArray)
-            //{
-            //    if ()
-            //}
-
-            return mid;
+            double middle = 0;
+            //int k = 0;
+            //index3 = 0;
+            int summBolt = 0;
+            foreach (int element in boxArray)
+            {
+                summBolt += element;
+            }
+            middle = Convert.ToDouble(summBolt) / boxArray.Length;
+            double difference;
+            double minDifference = middle;
+            int middleBox = 0;
+            for (int element = 0; element < boxArray.Length; element++)
+            {
+                difference = Math.Abs(middle - element);
+                if (difference < minDifference)
+                {
+                    minDifference = difference;
+                    middleBox = element; 
+                }
+                //if (difference = minDifference)
+                //{
+                //    minDifference = difference;
+                //    middleBox = element;
+                //}
+            }
+            //Console.WriteLine(middleBox);
+            return middleBox;
         }
     }
 }
